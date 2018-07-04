e79c24e8eaade400492bb919cbdec2fad8dfeea3
Jagepard <dankorot@gmail.com>
2018-07-04 13:36:25 +0300

    Updated

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 [![Build Status](https://travis-ci.org/Jagepard/PhpDesignPatterns-Decorator.svg?branch=master)](https://travis-ci.org/Jagepard/PhpDesignPatterns-Decorator)
+[![codecov](https://codecov.io/gh/Jagepard/PhpDesignPatterns-Decorator/branch/master/graph/badge.svg)](https://codecov.io/gh/Jagepard/PhpDesignPatterns-Decorator)
 [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Jagepard/PhpDesignPatterns-Decorator/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/Jagepard/PhpDesignPatterns-Decorator/?branch=master)
-[![Maintainability](https://api.codeclimate.com/v1/badges/7a6e4faef5f3bedee182/maintainability)](https://codeclimate.com/github/Jagepard/PhpDesignPatterns-Decorator/maintainability)
-
-![Decorator](https://github.com/Jagepard/PhpDesignPatterns-Decorator/blob/master/Decorator.png?raw=true "Decorator")
-
+[![Code Climate](https://codeclimate.com/github/Jagepard/PhpDesignPatterns-Decorator/badges/gpa.svg)](https://codeclimate.com/github/Jagepard/PhpDesignPatterns-Decorator)
+[![License: MIT](https://img.shields.io/badge/license-MIT-498e7f.svg)](https://mit-license.org/)
+-----
 
 # Decorator
 Декоратор
--- tests/DecoratorTest.php
+++ tests/DecoratorTest.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 /**
  * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2018, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ * @license   https://mit-license.org/ MIT
  */
 
-use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
+namespace Structural\Decorator\Tests;
+
 use Structural\Decorator\Component;
 use Structural\Decorator\XmlDecorator;
 use Structural\Decorator\JsonDecorator;
-
+use PHPUnit\Framework\TestCase as PHPUnit_Framework_TestCase;
 
 /**
  * Class DecoratorTest
@@ -32,26 +32,18 @@ class DecoratorTest extends PHPUnit_Framework_TestCase
 
     public function testComponent()
     {
-        $this->assertCount(1, $this->getComponent()->getData());
+        $this->assertCount(1, $this->component->getData());
     }
 
     public function testXmlDecorator()
     {
-        $xmlDecorator = new XmlDecorator($this->getComponent());
+        $xmlDecorator = new XmlDecorator($this->component);
         $this->assertEquals("<?xml version=\"1.0\"?>\n<root><key>value</key></root>\n", $xmlDecorator->getData());
     }
 
     public function testJsonDecorator()
     {
-        $jsonDecorator = new JsonDecorator($this->getComponent());
+        $jsonDecorator = new JsonDecorator($this->component);
         $this->assertEquals('{"key":"value"}', $jsonDecorator->getData());
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getComponent()
-    {
-        return $this->component;
     }
 }
